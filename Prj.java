561e51db7c8559c548dbcedf898c5c12130d3a5d
juiesable <jui.sable@gmail.com>
2017-03-12 00:43:28 +0530

    Add files via upload

---
+++ Prj.java
@@ -0,0 +1,8 @@
+public class Prj {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+System.out.println("welcome to jenkins server");
+
+   }
+}
